59a87130ccea459deaa993f90101100af6c46daa
zakiburnama <zaki.burnama1017@gmail.com>
2021-12-25 09:46:59 +0700

    parsing data json

--- App.js
+++ App.js
@@ -1,112 +1,62 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-// halo
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {Component} from 'react';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { FlatList, Text, TouchableHighlight, View, ToastAndroid, } from 'react-native';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      data:[]
+    }
+  }
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  componentDidMount(){
+    this.getData();
+  }
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  getData = () => {
+    fetch('https://gist.githubusercontent.com/erdem/8c7d26765831d0f9a8c62f02782ae00d/raw/248037cd701af0a4957cce340dabb0fd04e38f4c/countries.json')
+    .then((response) => response.json())
+    .then((json) => {
+      this.setState({data: json});
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+  pressBtn(tulisan) {
+    ToastAndroid.showWithGravityAndOffset(`BENAR ${tulisan}`, ToastAndroid.SHORT, ToastAndroid.BOTTOM, 25, 50);
+  }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  renderItem=({item}) => (
+            <TouchableHighlight
+            onPress={() => this.pressBtn(item.name)}
+            style={{borderBottomWidth:1, borderBottomColor:'#75AAF0'}}
+            >
+              <View style={{margin:10}}>
+                <Text style={{fontSize:15, fontFamily: 'sans-serif-condensed', textAlign:'center'}}>Negara : {item.name}</Text>
+                <Text style={{fontSize:15, fontFamily: 'sans-serif-condensed', textAlign:'center'}}>Kode Negara : {item.country_code}</Text>
+                <Text style={{fontSize:15, fontFamily: 'sans-serif-condensed', textAlign:'center'}}>Ibu Kota : {item.capital}</Text>
+                <Text style={{fontSize:15, fontFamily: 'sans-serif-condensed', textAlign:'center'}}>Lat : {item.latlng[0]}</Text>
+                <Text style={{fontSize:15, fontFamily: 'sans-serif-condensed', textAlign:'center'}}>Lng : {item.latlng[1]}</Text>
+                </View>
+            </TouchableHighlight>
+          );
+
+  render(){
+    return (
+      <View style={{flex:1, justifyContent:'center', backgroundColor: '#8FDAFA'}}>
+        <Text style={{backgroundColor: '#A3E3FA', textAlign:'center', fontSize:25, padding:10, fontFamily:'sans-serif-medium'}}>Countries List</Text>
+        <FlatList
+          data={this.state.data}
+          renderItem = {this.renderItem}
+          keyExtractor={(item) => item.name}
+        />
+        <Text style={{backgroundColor: '#A3E3FA', textAlign:'center', fontSize:15, padding:10, fontFamily:'sans-serif-medium'}}>Kelompok Delta</Text>
+      </View>
+    );
+  }
+}
 
 export default App;
